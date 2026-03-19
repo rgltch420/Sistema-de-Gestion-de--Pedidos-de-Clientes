921c31157c7f5c2a9d371a28d7f01c81485f61fa
rgltch420 <rmelendez.ruiz@icloud.com>
2026-03-19 09:06:23 -0500

    Creacion de usuarios con id y email

---
+++ src/modules/clientes.py
@@ -0,0 +1,53 @@
+import os
+import re
+
+client = {}
+
+def is_valid_email(email):
+    pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+    return re.match(pattern, email)
+
+def createClients():
+    tries = 0
+    id_client = 0
+    while(True):
+        try:
+            tries_users = input("Cuantos usarios deseas ingresar: ").strip()
+            tries_user_converted = int(tries_users)
+            if tries_user_converted < 1:
+                print("debe ser al menos 1")
+                continue
+            break
+        except (ValueError,UnboundLocalError):
+            print("Ingresa unicamente numeros positivos mayor a cero")   
+        
+    while tries_user_converted > tries :
+        tries += 1 
+        id_client +=1 
+        name_client = input("Ingresa el nombre del cliente: ").strip()
+        email_client = input("Ingresa el email del cliente: ").strip()
+        if not (name_client):
+            print("No dejar campos vacios o espacios vacios")
+            tries -= 1
+            id_client -= 1
+            continue
+        if not is_valid_email(email_client):
+            print("Ingresa un email válido (ej: xxxxx@xxxx.com)")
+            tries -= 1
+            id_client -= 1
+            continue     
+        client[id_client]={
+        "Name": name_client,
+        "Email": email_client
+    }
+        print(f"Cliente {client[id_client]['Name']} fue creado exitosamente con su mail {client[id_client]['Email']}")
+        
+        return id_client
+        
+       
+    else:
+        print("Ingresaste correctamente todos los usuarios")
+        
+           
+  
+createClients()  
